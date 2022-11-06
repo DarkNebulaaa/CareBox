b8a172f959b8addf72db0aacb5e6bcc81144c864
DarkNebulaaa <84328686+DarkNebulaaa@users.noreply.github.com>
2022-11-06 22:13:49 +0800

    ㄇㄠ

---
+++ App.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import { StatusBar } from 'react-native';
+import { Text, View, Image  ,TouchableOpacity,SafeAreaView} from 'react-native';
+import AnimatedLoader from 'react-native-animated-loader';
+
+
+
+import { useEffect } from 'react';
+import { useState } from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+
+import Loading from './src/Loading/Loading';
+import Home from './src/Home/Home'
+const Stack = createStackNavigator();
+
+/*########################################################
+ #                  MQTT   varible  Here                 #
+##########################################################*/
+const topicsub = 'esp32/app';
+const topic = 'esp32/app';
+const mqttAddr = 'fc9f0b02-internet-facing-7829070156367d76.elb.us-east-1.amazonaws.com';
+const mqttPort = 8083 ;
+
+
+/*########################################################
+ #                   Function Here                       #
+##########################################################*/
+
+
+
+/*########################################################
+##########################################################
+##########################################################*/ 
+export default function App() {
+  
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Loading">
+        
+        <Stack.Screen 
+        name="CareBox" 
+        component={Home} 
+        
+        />
+
+        <Stack.Screen 
+        name="Loading"  
+        component={Loading} 
+        options={{headerShown : false}}
+        />
+
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+
