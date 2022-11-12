704c3fc654ba19f3c51e38be16de183f11d76e0d
DarkNebulaaa <84328686+DarkNebulaaa@users.noreply.github.com>
2022-11-12 14:54:01 +0800

    format the code

--- src/components/TimeCard.js
+++ src/components/TimeCard.js
@@ -1,43 +1,57 @@
-import React, {useRef ,useState} from 'react';
-import {Animated, View, StyleSheet, Text ,StatusBar ,DatePickerIOS ,ScrollView } from 'react-native';
-import { Button } from '@rneui/themed';
-import { Card } from 'react-native-elements';
-import AnimatedCard from '../components/AnimatedCard';
-import { renderNode } from '@rneui/base';
+import React, { useRef, useState } from "react";
+import {
+  Animated,
+  View,
+  StyleSheet,
+  Text,
+  StatusBar,
+  DatePickerIOS,
+  ScrollView,
+} from "react-native";
+import { Button } from "@rneui/themed";
+import { Card } from "react-native-elements";
+import AnimatedCard from "../components/AnimatedCard";
+import { renderNode } from "@rneui/base";
 
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
-const TimeCard = props => {
+const TimeCard = (props) => {
+  /*@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+@@@@@@@@@@@@@@@@@@@     Define Value     @@@@@@@@@@@@@@@@@@@@@
+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ */
+
+  // State Value
   const [chosenDate, setChosenDate] = useState(new Date());
-  const [timeItem ,setTimeItem] = useState([]);
+  const [timeItem, setTimeItem] = useState([]);
+  //Animation Value
   const Value = useRef(new Animated.Value(-500)).current;
-  
   const ButtonValue = useRef(new Animated.Value(-310)).current;
+  //JSON Struct
 
+  /*@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+@@@@@@@@@@@@@@@@@@@    Function Here    @@@@@@@@@@@@@@@@@@@@@@
+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ */
   const AddTime = () => {
     //const time = chosenDate.substring(10 ,14)
-    console.log(chosenDate);
-    setTimeItem([...timeItem , chosenDate]);
+
+    setTimeItem([...timeItem, chosenDate]);
     DeleteCard();
   };
-
   const DeleteTime = (index) => {
     let TimeCopy = [...timeItem];
-    TimeCopy.splice(index , 1);
+    TimeCopy.splice(index, 1);
     setTimeItem(TimeCopy);
   };
-  
-  
-  const AddCard =() =>{
+  const AddCard = () => {
     AddButton();
     Animated.timing(Value, {
-      delay:200,
+      delay: 200,
       toValue: 0,
       duration: 500,
       useNativeDriver: true,
     }).start();
-    
   };
-  const DeleteCard =() =>{
+  const DeleteCard = () => {
     DeleteButton();
     Animated.timing(Value, {
       toValue: -500,
@@ -45,162 +59,206 @@ const TimeCard = props => {
       useNativeDriver: true,
     }).start();
   };
-
-  const AddButton =() =>{
-    
+  const AddButton = () => {
     Animated.timing(ButtonValue, {
       toValue: 0,
       duration: 500,
       useNativeDriver: true,
     }).start();
-    
   };
-  const DeleteButton =() =>{
-    
+  const DeleteButton = () => {
     Animated.timing(ButtonValue, {
-      delay:200,
+      delay: 200,
       toValue: -310,
       duration: 500,
       useNativeDriver: true,
     }).start();
   };
+  const storeData = async (index, hour, min) => {
+    try {
+      await AsyncStorage.setItem(
+        "＠ＣareBox:time:" + index,
+        "T" + hour + ":" + min
+      );
+    } catch (error) {
+      // 儲存錯誤
+      console.log("error: err to  storeData");
+    }
+  };
+  const getData = async (index) => {
+    try {
+      const value = await AsyncStorage.getItem("＠ＣareBox:time:" + index);
+      if (value !== null) {
+        // 有值！
+        console.log(value);
+      }
+    } catch (error) {
+      // 取值錯誤
+      console.log("error: err to get!!");
+    }
+  };
 
-  
+  /*@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+@@@@@@@@@@@@@@@@@@@    Return the frontend    @@@@@@@@@@@@@@@@@@
+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ */
+
   return (
     <View>
-      <StatusBar barStyle={'dark-content'}/>
+      <StatusBar barStyle={"dark-content"} />
       <Animated.View
         style={{
-          transform: [{translateX: Value}],
-          
+          transform: [{ translateX: Value }],
         }}
       >
-                <Card  borderRadius={20} >
-                    <View >
-                    <DatePickerIOS
-                            date={chosenDate}
-                            onDateChange={setChosenDate}
-                            mode={'time'}
-                            
-                            
-                      />
-                    </View>
-                    <View style={{flexDirection:'row' ,alignItems:'center', justifyContent:'center'}}>
-                      <View style={{paddingRight:100}}>
-                        <Button buttonStyle={{ borderRadius: 25, backgroundColor :'ffffff',borderColor:'rgba(111,206,182,1)',borderWidth:2}}
-                        onPress={DeleteCard}>
-                          <Text style={{fontSize:20 ,color:'rgba(111,206,182,1)'}}>
-                            Cancel
-                          </Text>
-                        </Button>
-                      </View>
-                      
-                      <Button buttonStyle={{ borderRadius: 25,backgroundColor :'rgba(111,206,182,1)',borderColor:'rgba(111,206,182,1)',borderWidth:2}}
-                      onPress ={AddTime}>
-                      <Text style={{fontSize:20 ,color:'rgba(255,255,255,1)'}}>
-                            Confirm
-                          </Text>
-                      </Button>
+        <Card borderRadius={20}>
+          <View>
+            <DatePickerIOS
+              date={chosenDate}
+              onDateChange={setChosenDate}
+              mode={"time"}
+            />
+          </View>
+          <View
+            style={{
+              flexDirection: "row",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            <View style={{ paddingRight: 100 }}>
+              <Button
+                buttonStyle={{
+                  borderRadius: 25,
+                  backgroundColor: "ffffff",
+                  borderColor: "rgba(111,206,182,1)",
+                  borderWidth: 2,
+                }}
+                onPress={DeleteCard}
+              >
+                <Text style={{ fontSize: 20, color: "rgba(111,206,182,1)" }}>
+                  Cancel
+                </Text>
+              </Button>
+            </View>
 
-                    </View>
-                    
-                </Card>
-
+            <Button
+              buttonStyle={{
+                borderRadius: 25,
+                backgroundColor: "rgba(111,206,182,1)",
+                borderColor: "rgba(111,206,182,1)",
+                borderWidth: 2,
+              }}
+              onPress={AddTime}
+            >
+              <Text style={{ fontSize: 20, color: "rgba(255,255,255,1)" }}>
+                Confirm
+              </Text>
+            </Button>
+          </View>
+        </Card>
       </Animated.View>
-      
+
       <Animated.View
         style={{
-          transform: [{translateY: ButtonValue}],
-          
+          transform: [{ translateY: ButtonValue }],
         }}
-      > 
-      <View style ={{marginTop: 20 ,marginLeft:10,marginRight:10 }}>
-                    <Button  
-                        title='Add Time' 
-                        buttonStyle ={{
-                          borderRadius: 20,
-                          backgroundColor: 'rgba(111,206,182,1)' 
-                        }}
-                        onPress ={AddCard}/>
+      >
+        <View style={{ marginTop: 20, marginLeft: 10, marginRight: 10 }}>
+          <Button
+            title="Add Time"
+            buttonStyle={{
+              borderRadius: 20,
+              backgroundColor: "rgba(111,206,182,1)",
+            }}
+            onPress={AddCard}
+          />
         </View>
 
         <ScrollView>
-        {
-            timeItem.map((item , index) => {
-              const format = (min) => {
-                if(min <10)
-                {
-                  return('0' + min);
-                }
-                else return (min);
-              }
-              return(
-                <AnimatedCard key ={index} Hour={item.getHours()} Min = {format(item.getMinutes())}/>
-              );
-            })
-        }          
-        </ScrollView>
+          {timeItem.map((item, index) => {
+            const formatM = (min) => {
+              if (min < 10) {
+                return "0" + min;
+              } else return min;
+            };
 
-      </Animated.View>                  
-      
-      
+            const formatH = (hour) => {
+              if (hour < 10) {
+                return "0" + hour;
+              } else return hour;
+            };
+
+            storeData(
+              index,
+              formatH(item.getHours()),
+              formatM(item.getMinutes())
+            );
+            getData(index);
+
+            return (
+              <AnimatedCard
+                key={index}
+                Hour={formatH(item.getHours())}
+                Min={formatM(item.getMinutes())}
+              />
+            );
+          })}
+        </ScrollView>
+      </Animated.View>
     </View>
   );
 };
-
+/*@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+@@@@@@@@@@@@@@@@@@@    Style Sheet    @@@@@@@@@@@@@@@@@@@@@@@@
+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ */
 
 const styles = StyleSheet.create({
   btnText: {
-    textAlign: 'center',
-    backgroundColor: '#aaa',
+    textAlign: "center",
+    backgroundColor: "#aaa",
     marginVertical: 10,
   },
   Detail: {
-    flexDirection:"row",
-    
-},
-closeImage: {
-    
-    width: 26, 
+    flexDirection: "row",
+  },
+  closeImage: {
+    width: 26,
     height: 26,
-    alignItems:'center',
-    justifyContent:'center',
-    
-},
-clockImage: {
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  clockImage: {
     marginTop: 6,
     marginLeft: 5,
-    width: 25, 
+    width: 25,
     height: 25,
-    
-},
-TimeText:{
-    color: 'aaaaaa',
+  },
+  TimeText: {
+    color: "aaaaaa",
     fontSize: 32,
-    fontWeight: 'lighter',
+    fontWeight: "lighter",
     marginTop: 0,
     paddingLeft: 20,
-},
-TimeNum:{
-    color: 'gray',
+  },
+  TimeNum: {
+    color: "gray",
     fontSize: 32,
-    fontWeight: 'lighter',
+    fontWeight: "lighter",
     marginTop: 0,
     paddingLeft: 20,
-},
-Button:{
-    borderRadius:10,
-    paddingLeft:0,
-    width: 40, 
+  },
+  Button: {
+    borderRadius: 10,
+    paddingLeft: 0,
+    width: 40,
     height: 40,
-    flex:1,
-    backgroundColor:"fffff",
-    
-},
-container: {
-  flex: 1,
-  justifyContent: 'center',
-},
+    flex: 1,
+    backgroundColor: "fffff",
+  },
+  container: {
+    flex: 1,
+    justifyContent: "center",
+  },
 });
 
 export default TimeCard;
